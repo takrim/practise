391ee98256178b8bd734f7526b2c4bd0a38c9170
Takrim Ayaaz Khan <takrimk@microsoft.com>
2017-04-13 06:58:20 -0700

    Adding Copy constructor

--- CompileMe.cpp
+++ CompileMe.cpp
@@ -4,6 +4,8 @@
 int main()
 {
   tk::Vector vector = {4, 3, 2, 53, 33, 22};
-  std::cout << "Hello World : " << std::endl;
+  tk::Vector vector1(vector);
+	vector.m_elem[0] = 9;
+  std::cout << "Hello World : " << vector1.m_elem[0] << ":" << vector.m_elem[0] << std::endl;
   return 0;
 }
--- VectorPractise.h
+++ VectorPractise.h
@@ -7,9 +7,14 @@ class Vector
 {
 private:
   unsigned int m_size;
-  double *m_elem;
+  
 
 public:
+	double* m_elem;
+	unsigned int vectorsize() const 
+	{
+		 return m_size;
+	}
   Vector(unsigned int size) : m_size{size}, m_elem{new double[size]}
   {
   }
@@ -17,6 +22,12 @@ public:
   {
     std::copy(list.begin(), list.end(), m_elem);
   }
+
+	Vector(const Vector& vector) : m_size{vector.vectorsize()}, m_elem{new double[m_size]}
+	{
+		std::copy(vector.m_elem, vector.m_elem + m_size,m_elem);
+	}
+
   ~Vector()
   {
     delete[] m_elem;
