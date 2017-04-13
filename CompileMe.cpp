bab05ecd18664c47b8adabf3adf68e665471f87c
Takrim Ayaaz Khan <takrimk@microsoft.com>
2017-04-12 23:14:24 -0700

    Fixing indent

--- CompileMe.cpp
+++ CompileMe.cpp
@@ -1,10 +1,9 @@
-#include<iostream>
+#include <iostream>
 #include "VectorPractise.h"
 
 int main()
 {
-	tk::Vector vector = {4,3,2,53,33,22};
-
-	std::cout << "Hello World : " << std::endl;   
-	return 0;
+  tk::Vector vector = {4, 3, 2, 53, 33, 22};
+  std::cout << "Hello World : " << std::endl;
+  return 0;
 }
