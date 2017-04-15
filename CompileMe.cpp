e4ae2b2a6ed05f12ba6546595eabbf0b2b48dbd4
Takrim Ayaaz Khan <takrimk@microsoft.com>
2017-04-15 07:42:04 -0700

    Adding pushback to the vector

--- CompileMe.cpp
+++ CompileMe.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include "VectorPractise.h"
-
+void printVector(const tk::Vector& vector);
 int main()
 {
-  tk::Vector vector = {4, 3, 2, 53, 33, 22};
-  tk::Vector vector1(vector);
-	vector.m_elem[0] = 9;
-	tk::Vector vector2 = {5, 3, 2, 5};
-	vector2 = vector1;
-	std::cout << "Hello World : " <<  vector2.m_elem[0] << ":" << vector2.vectorsize() << std::endl;
-	vector2 = vector;
-	std::cout << "Hello World : " <<  vector2.m_elem[0] << ":" << vector2.vectorsize() << std::endl;
-  std::cout << "Hello World : " << vector1.m_elem[0] << ":" << vector.m_elem[0] << std::endl;
-  return 0;
+    tk::Vector vector = {4, 3, 2, 53, 33, 22};
+    vector.pushback(32);
+    vector.pushback(311);
+    vector.pushback(312);
+    vector.pushback(3212);
+    printVector(vector);
+    // tk::Vector vector1(vector);
+    // vector.m_elem[0] = 9;
+    // {
+    // tk::Vector vector2 = {5, 3, 2, 5};
+    // vector = vector1;
+    // }
+    // tk::Vector vectormovtest(tk::Vector({4,5}));
+    // {
+    //     tk::Vector vectormoved = {4, 2, 3, 2};
+    //     vectormovtest = vectormoved;
+    // }
+    // std::cout << "Hello World : " << vector2.m_elem[0] << ":" << vector2.vectorsize() << std::endl;
+    // vector2 = vector;
+    // std::cout << "Hello World : " << vector2.m_elem[0] << ":" << vector2.vectorsize() << std::endl;
+    // std::cout << "Hello World : " << vector1.m_elem[0] << ":" << vector.m_elem[0] << std::endl;
+    return 0;
 }
+void printVector(const tk::Vector& vector)
+{
+    for(int i=0; i<vector.vectorsize(); i++)
+    {
+        std::cout << vector.m_elem[i] << ",";
+    }
+    std::cout << " size:" << vector.vectorsize() << std::endl; 
+};
+// void checkmovedconstructor
--- VectorPractise.h
+++ VectorPractise.h
@@ -1,45 +1,99 @@
 #include <initializer_list>
 #include <algorithm>
+#include <iostream>
 namespace tk
 {
 
 class Vector
 {
-private:
-  unsigned int m_size;
-  
+  private:
+    unsigned int m_size;
+    unsigned int m_space;
+    void reserve(int newmalloc)
+    {
+        if (newmalloc <= m_space)
+        {
+            return;
+        }
+        double *p = new double[newmalloc];
+        for (int i = 0; i < m_size; i++)
+        {
+            p[i] = m_elem[i];
+        }
+        delete[] m_elem;
+        m_elem = p;
+        m_space = newmalloc;
+    }
 
-public:
-	double* m_elem;
-	unsigned int vectorsize() const 
-	{
-		 return m_size;
-	}
-  Vector(unsigned int size) : m_size{size}, m_elem{new double[size]}
-  {
-  }
-  Vector(std::initializer_list<double> list) : m_size{list.size()}, m_elem{new double[m_size]}
-  {
-    std::copy(list.begin(), list.end(), m_elem);
-  }
+  public:
+    double *m_elem;
+    void pushback(double value)
+    {
+        if (m_size == 0)
+        {
+            reserve(8);
+        }
+        if (m_size == m_space)
+        {
+            reserve(2 * m_space);
+        }
+        m_elem[m_size ++] = value;
+    }
+    unsigned int vectorsize() const
+    {
+        return m_size;
+    }
 
-	Vector(const Vector& vector) : m_size{vector.vectorsize()}, m_elem{new double[m_size]}
-	{
-		std::copy(vector.m_elem, vector.m_elem + m_size,m_elem);
-	}
-	Vector& operator = (const Vector& copy)
-	{
-		double* tmp = new double[copy.vectorsize()];
-		std::copy(copy.m_elem, copy.m_elem + copy.vectorsize(), tmp);
-		delete[] m_elem;
-		m_elem = tmp;
-		m_size = copy.vectorsize();
-		
-		return *this;
-	}
-  ~Vector()
-  {
-    delete[] m_elem;
-  }
+    void vectorsize(unsigned int size)
+    {
+        m_size = size;
+    }
+
+    Vector(unsigned int size) : m_size{size}, m_elem{new double[size]}
+    {
+    }
+
+    Vector(std::initializer_list<double> list) : m_size{list.size()}, m_space{m_size}, m_elem{new double[m_size]}
+    {
+        std::copy(list.begin(), list.end(), m_elem);
+    }
+
+    Vector(const Vector &vector) : m_size{vector.vectorsize()}, m_space{m_size}, m_elem{new double[m_size]}
+    {
+        std::cout << "Copy constructor called" << std::endl;
+        std::copy(vector.m_elem, vector.m_elem + m_size, m_elem);
+    }
+    Vector &operator=(const Vector &copy)
+    {
+        double *tmp = new double[copy.vectorsize()];
+        std::copy(copy.m_elem, copy.m_elem + copy.vectorsize(), tmp);
+        delete[] m_elem;
+        m_elem = tmp;
+        m_size = copy.vectorsize();
+        m_space = m_size;
+        std::cout << "Copy assignment operator called" << std::endl;
+        return *this;
+    }
+    Vector(Vector &&vector) : m_size{vector.vectorsize()}, m_space{m_size}, m_elem{new double[m_size]}
+    {
+        std::cout << "Move constructor called" << std::endl;
+        std::copy(vector.m_elem, vector.m_elem + m_size, m_elem);
+    }
+    Vector &operator=(Vector &&vector)
+    {
+        delete[] m_elem;
+        m_size = vector.vectorsize();
+        m_space = m_size;
+        m_elem = vector.m_elem;
+        vector.m_elem = nullptr;
+        vector.vectorsize(0);
+        std::cout << "Move assignment operator called" << std::endl;
+
+        return *this;
+    }
+    ~Vector()
+    {
+        delete[] m_elem;
+    }
 };
 }
