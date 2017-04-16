c14621ea59de84b343a1c6b3fc9ed5e10cd6904c
Takrim Ayaaz Khan <takrim@gmail.com>
2017-04-16 09:52:36 -0700

    Moving implementation out of header for tk::Vector Fixes#4

--- VectorPractise.h
+++ VectorPractise.h
@@ -1,11 +1,31 @@
 #include <initializer_list>
-#include <algorithm>
-#include <iostream>
 namespace tk
 {
-
 class Vector
 {
+  public:
+
+    Vector(unsigned int size);
+    Vector(std::initializer_list<double> list);
+    Vector(const Vector &vector);
+    Vector(Vector&& vector);
+
+
+    void pushback(double value);
+    unsigned int vectorsize() const;
+    void vectorsize(unsigned int size);
+
+
+    Vector &operator=(const Vector &copy);
+    double &operator [] (int n);
+    double &operator [] (int n) const;
+
+    Vector &operator=(Vector&& vector);
+    ~Vector()
+    {
+        delete[] m_elem;
+    }
+
   private:
     unsigned int m_size;
     unsigned int m_space;
@@ -26,84 +46,5 @@ class Vector
     }
 
     double *m_elem;
-  public:
-    void pushback(double value)
-    {
-        if (m_size == 0)
-        {
-            reserve(8);
-        }
-        if (m_size == m_space)
-        {
-            reserve(2 * m_space);
-        }
-        m_elem[m_size ++] = value;
-    }
-    unsigned int vectorsize() const
-    {
-        return m_size;
-    }
-
-    void vectorsize(unsigned int size)
-    {
-        m_size = size;
-    }
-
-    Vector(unsigned int size) : m_size{size}, m_elem{new double[size]}
-    {
-    }
-
-    Vector(std::initializer_list<double> list) : m_size{list.size()}, m_space{m_size}, m_elem{new double[m_size]}
-    {
-        std::copy(list.begin(), list.end(), m_elem);
-    }
-
-    Vector(const Vector &vector) : m_size{vector.vectorsize()}, m_space{m_size}, m_elem{new double[m_size]}
-    {
-        std::cout << "Copy constructor called" << std::endl;
-        std::copy(vector.m_elem, vector.m_elem + m_size, m_elem);
-    }
-    Vector &operator=(const Vector &copy)
-    {
-        double *tmp = new double[copy.vectorsize()];
-        std::copy(copy.m_elem, copy.m_elem + copy.vectorsize(), tmp);
-        delete[] m_elem;
-        m_elem = tmp;
-        m_size = copy.vectorsize();
-        m_space = m_size;
-        std::cout << "Copy assignment operator called" << std::endl;
-        return *this;
-    }
-    Vector(Vector&& vector) : m_size{vector.vectorsize()}, m_space{m_size}, m_elem{new double[m_size]}
-    {
-        std::cout << "Move constructor called" << std::endl;
-        std::copy(vector.m_elem, vector.m_elem + m_size, m_elem);
-    }
-
-    double &operator [] (int n)
-    {
-        return m_elem[n];
-    }
-    double &operator [] (int n) const
-    {
-        return m_elem[n];
-    }
-
-    Vector &operator=(Vector&& vector)
-    {
-        delete[] m_elem;
-        m_size = vector.vectorsize();
-        m_space = m_size;
-        m_elem = vector.m_elem;
-        vector.m_elem = nullptr;
-        vector.vectorsize(0);
-        std::cout << "Move assignment operator called" << std::endl;
-
-        return *this;
-    }
-    ~Vector()
-    {
-        delete[] m_elem;
-    }
 };
 }
